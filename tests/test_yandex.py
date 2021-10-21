6e33f9aa6e54721495d0429c6a1513c3fa4b1c47
91dante91 <kunanbaevmurat5@gmail.com>
2021-10-21 19:17:59 +0500

    Test tensor

--- tests/test_yandex.py
+++ tests/test_yandex.py
@@ -1,4 +1,5 @@
 from tensor_test import YandexSearch
+
 yandex = YandexSearch()
 
 
@@ -9,22 +10,28 @@ def test_search_in_yandex():
 def test_search_suggest():
     assert yandex.search_suggest() is True
 
+
 def test_search_links_in_result():
     links = yandex.search_links_in_result()
     assert 'tensor.ru' in links
+
 
 def test_check_link_yandex_image():
     link = yandex.check_link_yandex_image()
     assert 'https://yandex.ru/images/' in link
 
+
 def test_check_first_category():
     assert yandex.check_first_category() is True
+
 
 def test_check_open_first_img():
     assert yandex.open_first_image() is True
 
+
 def test_check_btn_next():
     assert yandex.check_button_next() is True
+
 
 def test_check_btn_prev():
     assert yandex.check_button_prev() is True
